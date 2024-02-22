c75e854d196fc1107554b448cd867f10d9ed3d89
yunjiniius <yunjiniius@gmail.com>
2024-02-22 13:19:19 +0900

    crud (select, async select)

---
+++ chap03-crud/src/main/java/com/ohgiraffers/crud/menu/controller/MenuController.java
@@ -0,0 +1,43 @@
+package com.ohgiraffers.crud.menu.controller;
+
+import com.ohgiraffers.crud.menu.model.dto.CategoryDTO;
+import com.ohgiraffers.crud.menu.model.dto.MenuDTO;
+import com.ohgiraffers.crud.menu.model.service.MenuService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/menu")
+public class MenuController {
+
+    private final MenuService menuService;
+
+    public MenuController(MenuService menuService) {
+        this.menuService = menuService;
+    }
+
+    @GetMapping("/list")
+    public String findMenuList(Model model) {
+
+        List<MenuDTO> menuList = menuService.findAllMenu();
+
+        model.addAttribute("menuList", menuList);
+
+        return "menu/list";
+    }
+
+    @GetMapping("/regist")
+    public void registPage() {}
+
+    @GetMapping(value = "category", produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    public List<CategoryDTO> findCategoryList() {
+        return menuService.findAllCategory();
+    }
+
+}
