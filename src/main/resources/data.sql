984bb547c52af322bf4a8ae37dfbb6651a2a63e9
mins199600 <mins19960091@gmail.com>
2024-02-25 21:53:02 +0900

    chore: sql파일 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO jdbctest.REPORT
+(REP_NO, REP_TITLE, REP_CONTENT, REP_DATE, REP_ID, REP_PRC)
+VALUES(1, '테스트제목입니','내용도테스트', '20240225','테스트아이', '처리');
+
+
+INSERT INTO jdbctest.REPORT
+(REP_NO, REP_TITLE, REP_CONTENT, REP_DATE, REP_ID, REP_PRC)
+VALUES(2, '테스트제목입니다22','내용도테스트22', '20240225','테스트아이디22', '처리');
